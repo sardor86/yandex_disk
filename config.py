0c7528f4e5d737c0f7635bf09b34bdf8f97f61dc
Sardor <sshavkatov880@gmail.com>
2022-12-02 21:49:53 +0300

    add main setings

---
+++ config.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+LOGIN_URL = "https://passport.yandex.ru/auth?retpath=https%3A%2F%2Foauth.yandex.ru%2Fauthorize%3Fresponse_type%3Dtoken%" \
+            "26client_id%3Df53cfda9e2ad4769863f72f63b959a3f&noreturn=1&origin=oauth"
+
+BASE_DIR = Path(__file__).resolve().parent
---
+++ yandex_disk/yandex_disk.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+import requests
+import json
+
+from config import LOGIN_URL
+import time
+
+
+class GetApiDisk(webdriver.Chrome):
+    def __init__(self, driver_path: str):
+        super().__init__(executable_path=driver_path)
+        print('Браузер готов')
+
+        self.user_name = None
+        self.user_password = None
+        self.disk_api = None
+
+    def get_api(self, user_name: str, user_password: str) -> bool:
+        ##########################################################################################################
+        # login
+        self.get(LOGIN_URL)
+        time.sleep(2)
+        self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]"
+                                    "/div[3]/div/div/div/div[1]/form/div[2]/div/div[2]"
+                                    "/span/input").send_keys(user_name)
+        time.sleep(3)
+
+        self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]"
+                                    "/div/div/div/div[1]/form/div[4]/button").click()
+        time.sleep(3)
+
+        try:
+            error = self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]"
+                                                "/div/div/div/div[1]/form/div[2]/div/div[2]/div")
+
+            print("Такого аккаунта не существует")
+            return False
+        except NoSuchElementException:
+            time.sleep(3)
+
+        ##################################################################################################
+        # password
+
+        self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]"
+                                    "/div/div/div/form/div[2]/div[1]/span/input").send_keys(user_password)
+
+        self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]"
+                                    "/div/div/div/form/div[3]/button").click()
+        time.sleep(3)
+
+        try:
+            error = self.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/div/div/div[2]/div[3]"
+                                                "/div/div/div/form/div[2]/div[1]/div")
+            print("Неверный пароль")
+            return False
+        except NoSuchElementException:
+            time.sleep(2)
+
+        self.switch_to.frame(self.find_element(By.XPATH, "/html/body/div[3]/div/div/span/section/div[1]/div[1]"
+                                                         "/div/section/div/div/section/div/div/div/div[1]/div/"
+                                                         "section/div/div/div/div[4]/section/div/div/iframe"))
+
+        self.disk_api = self.find_element(By.XPATH, "/html/body/div/section/div[1]/span/input").get_attribute('value')
+
+        with open("data.json", "w") as file:
+            data = {
+                    "user_name": user_name,
+                    "user_password": user_password,
+                    "user_token": self.disk_api
+                   }
+            file.write(json.dumps(data))
+
+        return True
+
+    def __del__(self):
+        self.close()
+        self.quit()
+        print("Браузер закрыт")
